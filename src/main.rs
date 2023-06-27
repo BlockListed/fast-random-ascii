b5c9f95b302bfdf6b1221e29eb6dc4af3e01bcf4
BlockListed <44610569+BlockListed@users.noreply.github.com>
2023-06-27 17:43:28 +0200

    tokio async rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,32 @@
 use rand::{Rng, SeedableRng, RngCore};
 use rand_xorshift::XorShiftRng;
 
-use glommio::{LocalExecutor, spawn_local, yield_if_needed};
-use glommio::channels::local_channel::{LocalReceiver, LocalSender, new_bounded};
-use glommio::io::{BufferedFile, StreamWriterBuilder};
-
-use futures_lite::AsyncWriteExt;
+use tokio::sync::mpsc::{channel, Sender, Receiver};
+use tokio::io::stdout;
+use tokio::io::AsyncWriteExt;
+use tokio::task::yield_now;
 
 const BUFFER_SIZE: usize = 4096;
 const BUFFER_AMOUNT: usize = 4;
 
-fn main() {
-    let (ascii_tx, ascii_rx) = new_bounded(BUFFER_AMOUNT);
-    let (mut buf_tx, buf_rx) = new_bounded(BUFFER_AMOUNT);
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let (ascii_tx, ascii_rx) = channel(BUFFER_AMOUNT);
+    let (mut buf_tx, buf_rx) = channel(BUFFER_AMOUNT);
 
-    LocalExecutor::default()
-        .run(async {
-            create_initial_buffers(&mut buf_tx).await;
-
-            spawn_local(generate_ascii(buf_rx, ascii_tx)).detach();
-            output_ascii(ascii_rx, buf_tx).await;
-        });
+    create_initial_buffers(&mut buf_tx).await;
+    tokio::spawn(generate_ascii(buf_rx, ascii_tx));
+    output_ascii(ascii_rx, buf_tx).await;
 }
 
-async fn create_initial_buffers(buf_tx: &mut LocalSender<Vec<u8>>) {
+async fn create_initial_buffers(buf_tx: &mut Sender<Vec<u8>>) {
     for _ in 0..BUFFER_AMOUNT {
         let buf = vec![0_u8; BUFFER_SIZE];
         buf_tx.send(buf).await.unwrap();
     }
 }
 
-async fn generate_ascii(buf_rx: LocalReceiver<Vec<u8>>, ascii_tx: LocalSender<Vec<u8>>) {
+async fn generate_ascii(mut buf_rx: Receiver<Vec<u8>>, ascii_tx: Sender<Vec<u8>>) {
     let mut generator = {
         let seed: u64 = rand::rngs::OsRng.gen();
         XorShiftRng::seed_from_u64(seed)
@@ -49,20 +45,21 @@ async fn generate_ascii(buf_rx: LocalReceiver<Vec<u8>>, ascii_tx: LocalSender<Ve
 
         ascii_tx.send(buf).await.unwrap();
 
-        yield_if_needed().await;
+        yield_now().await;
     }
 }
 
-async fn output_ascii(ascii_rx: LocalReceiver<Vec<u8>>, buf_tx: LocalSender<Vec<u8>>) {
-    let file = BufferedFile::open("/dev/stdout").await.unwrap();
-    let mut output = StreamWriterBuilder::new(file).with_buffer_size(BUFFER_SIZE).build();
+async fn output_ascii(mut ascii_rx: Receiver<Vec<u8>>, buf_tx: Sender<Vec<u8>>) {
+    let mut output = stdout();
 
     while let Some(buf) = ascii_rx.recv().await {
         output.write_all(&buf).await.unwrap();
 
+        output.flush().await.unwrap();
+
         buf_tx.send(buf).await.unwrap();
 
-        yield_if_needed().await;
+        yield_now().await;
     }
 }
 
